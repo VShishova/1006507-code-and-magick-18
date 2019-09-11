4a6d1c4ff2d793550fc92677f4185ad43f9b9766
VShishova <v.shish79@yandex.ru>
2019-09-11 10:04:42 +0300

    Правки

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var gulp = require("gulp");
+var server = require("browser-sync").create();
+
+gulp.task("server", function () {
+  server.init({
+    server: "",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+  gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("start", gulp.series("server"));
